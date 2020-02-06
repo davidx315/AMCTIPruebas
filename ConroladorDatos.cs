6b44a4e69c824299c860dbd0968e2ea0951efa12
davidx315 <45082617+davidx315@users.noreply.github.com>
2020-02-06 11:03:32 -0500

    Se agrega Productos.Core

---
+++ ConroladorDatos.cs
@@ -0,0 +1,55 @@
+﻿using AMCTI.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMCTIPrueba.Productos.Core
+{
+    public class ControladorDatos
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["ContextAMCTI"].ConnectionString;
+
+        public DataTable ObtenerProductos(int idProducto)
+        {
+            DataTable dtResultado = null;
+            var conexion = new DAO(connectionString); /*Instansiar un Objeto*/
+            conexion.Procedimiento = "SP_S_Producto";
+            dtResultado = conexion.ObtenerDataTable();
+
+            if (conexion.ErroresEnElProceso != null)
+            {
+                throw conexion.ErroresEnElProceso;
+            }
+
+            return dtResultado;
+        }
+
+
+
+        //Para alimentar el DropDown//
+        public DataSet ObtenerProducto2()
+        {
+            //Conectar con la base de datos//
+            DataSet dtResultado = null;
+            var conexion = new DAO(connectionString);
+            conexion.Procedimiento = "SP_S_Producto";
+            dtResultado = conexion.ObtenerDataSet();
+
+            if (conexion.ErroresEnElProceso != null)
+            {
+                throw conexion.ErroresEnElProceso;
+            }
+
+            return dtResultado;
+
+
+        }
+
+
+    }
+}
---
+++ ControladorListas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMCTIPrueba.Productos.Core
+{
+    using System.Data;
+    using System.Linq;
+    using System.Web.UI.WebControls;
+
+     public class ControladorListasProducto
+    {
+        public ControladorDatos objDatosProducto = new ControladorDatos();
+
+        public DropDownList ListadoTitulo(DropDownList lista)
+        {
+            return LlenarLista(lista, objDatosProducto.ObtenerProducto2(), "titulo", "idProd");
+        }
+
+        protected DropDownList LlenarLista(DropDownList lista, object dataSource, string texto, string valor)
+        {
+            lista.DataSource = dataSource;
+            lista.DataTextField = texto;
+            lista.DataValueField = valor;
+            lista.DataBind();
+            return lista;
+        }
+    }
+}
